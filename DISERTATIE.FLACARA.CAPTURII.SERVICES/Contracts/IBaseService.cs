c8156ff363ae87624270b746a2475c14b1217b74
defdddd <82803508+defdddd@users.noreply.github.com>
2024-03-24 08:28:16 +0200

    1.0

---
+++ DISERTATIE.FLACARA.CAPTURII.SERVICES/Contracts/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DISERTATIE.FLACARA.CAPTURII.SERVICES.Contracts;
+
+public interface IBaseService<T> where T : class
+{
+    Task<List<T>> Entities();
+    Task<T> InsertEntityAsync(T value);
+    Task<T> UpdateEntityAsync(T value);
+    Task<T> SearchEntityByIdAsync(int id);
+    Task<bool> DeleteEntityAsync(T value);
+}
